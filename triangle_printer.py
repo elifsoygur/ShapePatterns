f10b061914705fb133e55b8e01138340824542a0
elifsoygur <eliffsoygurr@gmail.com>
2025-06-03 14:18:47 +0300

    Update triangle_printer.py

--- triangle_printer.py
+++ triangle_printer.py
@@ -7,6 +7,11 @@ e= int(input("Please enter a number"))
 for i in range(1,e+1):
   print(" " * (e-i) + "*" * i)
 // Hollow triangle
-e= int(input("Please enter a number"))
-for i in range(1,e+1):
-  
+e = int(input("Please enter a number: "))
+for i in range(1, e + 1):
+    if i == 1:
+      print(" " * (e - i) + "*")
+    elif i == e:
+      print("*" * (2 * e - 1))
+    else:
+       print(" " * (e - i) + "*" + " " * (2 * i - 3) + "*")
