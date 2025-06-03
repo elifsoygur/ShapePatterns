5f5016702486f1b5aa441d2f32f41703bf3eeb79
elifsoygur <eliffsoygurr@gmail.com>
2025-06-03 13:49:52 +0300

    Create triangle_printer.py

---
+++ triangle_printer.py
@@ -0,0 +1,12 @@
+//Left-aligned triangle
+e= int(input("Please enter a number"))
+for i in range(1,e+1):
+  print("*" * i)
+//Right-aligned triangle
+e= int(input("Please enter a number"))
+for i in range(1,e+1):
+  print(" " * (e-i) + "*" * i)
+// Hollow triangle
+e= int(input("Please enter a number"))
+for i in range(1,e+1):
+  
